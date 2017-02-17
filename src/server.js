4735b131dbb62c4b50e7366104b21c90450516bc
Michael <mpl2999@rit.edu>
2017-02-17 14:53:49 -0500

    added shouting

--- src/server.js
+++ src/server.js
@@ -9,11 +9,11 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const onRequest = (request, response) => {
-  response.writeHead(200, {
-    'Content-Type': 'text/html',
-  });
-  response.write(index);
-  response.end();
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
+    response.write(index);
+    response.end();
 };
 
 const app = http.createServer(onRequest).listen(port);
@@ -25,67 +25,73 @@ const io = socketio(app);
 const users = {};
 
 const onJoined = (sock) => {
-  const socket = sock;
+    const socket = sock;
 
-  socket.on('join', (data) => {
-    users[data.name] = data.name;
+    socket.on('join', (data) => {
+        users[data.name] = data.name;
 
-    const joinMsg = {
-      name: 'server',
-      msg: `There are ${Object.keys(users).length} users online.`,
-    };
+        const joinMsg = {
+            name: 'server',
+            msg: `There are ${Object.keys(users).length} users online.`,
+        };
 
-    socket.name = data.name;
-    socket.emit('msg', joinMsg);
+        socket.name = data.name;
+        socket.emit('msg', joinMsg);
 
-    socket.join('room1');
+        socket.join('room1');
 
         // announcement to everyone in the room
-    const response = {
-      name: 'server',
-      msg: `${data.name} has joined the room`,
-    };
-    socket.broadcast.to('room1').emit('msg', response);
+        const response = {
+            name: 'server',
+            msg: `${data.name} has joined the room`,
+        };
+        socket.broadcast.to('room1').emit('msg', response);
 
-    console.log(`${data.name} joined`);
+        console.log(`${data.name} joined`);
         // success message back to new user
-    socket.emit('msg', {
-      name: 'server',
-      msg: 'You joined the room',
+        socket.emit('msg', {
+            name: 'server',
+            msg: 'You joined the room',
+        });
     });
-  });
 };
 
 const onMsg = (sock) => {
-  const socket = sock;
+    const socket = sock;
 
-  socket.on('msgToServer', (data) => {
-    io.sockets.in('room1').emit('msg', {
-      name: data.name,
-      msg: data.msg,
+    socket.on('msgToServer', (data) => {
+        io.sockets.in('room1').emit('msg', {
+            name: data.name,
+            msg: data.msg,
+        });
     });
-  });
+    
+    socket.on('shout', (data) => {
+       io.sockets.in('room1').emit('msg', {
+           name: data.name,
+           msg: data.msg.toUpperCase(),
+       });
+    });
 };
-
 const onDisconnect = (sock) => {
-  const socket = sock;
+    const socket = sock;
 
-  socket.on('disconnect', () => {
-    const response = {
-      name: 'server',
-      msg: `${socket.name} has left the room.`,
-    };
-    socket.broadcast.to('room1').emit('msg', response);
-    socket.leave('room1');
+    socket.on('disconnect', () => {
+        const response = {
+            name: 'server',
+            msg: `${socket.name} has left the room.`,
+        };
+        socket.broadcast.to('room1').emit('msg', response);
+        socket.leave('room1');
 
-    delete users[socket.name];
-  });
+        delete users[socket.name];
+    });
 };
 
 io.sockets.on('connection', (socket) => {
-  onJoined(socket);
-  onMsg(socket);
-  onDisconnect(socket);
+    onJoined(socket);
+    onMsg(socket);
+    onDisconnect(socket);
 });
 
 console.log('Websocket server has started');
